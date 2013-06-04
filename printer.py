56a8e99b9def2decf6da316118330addce2c49ce
johnnyshi1225 <841471958@qq.com>
2013-06-04 22:59:13 +0800

    Init project.

---
+++ printer.py
@@ -0,0 +1,9 @@
+class printer:
+    def __init__(self):
+        '''
+        I'm a printer.
+        '''
+        self.tag = 1
+
+    def printSomething(self):
+        print 'Something that I want to print.'
